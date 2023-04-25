98ddca3bdc940946dd296eba085b84ae04f749b3
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-04-25 22:55:09 +0300

    completed

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    console.log(`Comparing ${word[startIndex]} and ${word[endIndex]}`);
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+console.log(isPalindrome("racecar")); // Expecting: true
+console.log(isPalindrome("hello")); // Expecting: false
+console.log(isPalindrome("a")); // Expecting: true
+console.log(isPalindrome("robot")); // Expecting: false
+console.log(isPalindrome("ab")); 
